967f29366646a0b0501ab033e3bc0d42b0d6fc0e
Raghav Sharma <raghu250407@gmail.com>
2024-04-07 00:37:08 +0530

    created app component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+};
 
-export default App
+export default App;
