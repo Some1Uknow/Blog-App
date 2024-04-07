71f90187df2f5c6046825322d563301743ed0e50
Raghav Sharma <raghu250407@gmail.com>
2024-04-07 17:26:53 +0530

    added header in home

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -3,14 +3,14 @@ import { MdComputer } from "react-icons/md";
 const Home = () => {
   return (
     <>
-      <div className="flex justify-between py-10 px-40">
+      <div className="flex justify-between py-5 px-20 bg-gray-100 mb-5">
         <p className="flex flex-row items-center text-4xl font-semibold">
           <MdComputer className="mr-2" />
           TechReads
         </p>
-        <div className="flex gap-4">
-          <button className="btn">Login</button>
-          <button className="btn">Register</button>
+        <div className="flex gap-4 ">
+          <button className=" text-black font-normal">Login</button>
+          <button className="btn text-white font-normal">Register</button>
         </div>
       </div>
     </>
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,5 +1,33 @@
+import Header from "./Header";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-screen">
+      <Header />
+      <div className="px-10 relative h-full">
+        <div
+          className="h-4/5 flex items-end justify-start relative"
+          style={{
+            backgroundImage: 'url("./public/home-bg.jpg")',
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            borderRadius: "30px",
+          }}
+        >
+          <div className="absolute inset-0 bg-black opacity-50 rounded-3xl"></div>
+          <div className="absolute bottom-0 left-0 p-8">
+            <h1 className="text-4xl font-bold text-white mb-4">
+              The place to share ideas that connect technocrats, inspire
+              innovation, and drive technological advancement.
+            </h1>
+            <p className="text-lg text-white">
+              Explore the latest insights, trends, and innovations in the world
+              of technology.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
