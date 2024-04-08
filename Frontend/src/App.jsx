2be34da18242b11592bfcae487925bfc0a490a87
Raghav Sharma <raghu250407@gmail.com>
2024-04-08 23:19:37 +0530

    added new font

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import BlogList from "./components/BlogList";
 
 function App() {
   return (
-    <div className="bg-gray-50 h-full text-black">
+    <div className="bg-gray-50 h-full text-black font-NotoSans">
       <Home />
       <BlogList />
     </div>
