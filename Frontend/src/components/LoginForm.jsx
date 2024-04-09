1204cf727f73456ce292467d4ad150d8c99e5aea
Raghav Sharma <raghu250407@gmail.com>
2024-04-09 23:25:22 +0530

    added framer motion

--- Frontend/src/components/LoginForm.jsx
+++ Frontend/src/components/LoginForm.jsx
@@ -1,47 +1,73 @@
 import { MdComputer } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function LoginForm() {
   return (
     <div className="w-screen h-screen flex flex-row justify-between bg-gray-200">
       <div className="flex flex-row justify-center w-1/2 items-center">
         <div className="flex items-center h-screen w-3/4 ">
-          <div className="w-full rounded shadow-lg p-8 m-4 bg-white flex flex-col items-center">
-            <span className="block w-full text-2xl text-black uppercase font-bold mb-4">
-              Login
-            </span>
-            <form
-              className="mb-4 w-full flex flex-col items-center"
-              action="/"
-              method="post"
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 50 }}
+              transition={{ duration: 0.5 }}
+              className="w-full rounded shadow-lg p-8 m-4 bg-white flex flex-col items-center"
             >
-              <div className="mb-4 w-full">
-                <input
-                  className="input w-full border-b-2 p-2 outline-none focus:shadow-outline bg-gray-100"
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Username or Email"
-                />
-              </div>
-              <div className="mb-6 w-full">
-                <input
-                  className="input w-full border-b-2 p-2 outline-none focus:shadow-outline bg-gray-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Password"
-                />
-              </div>
-              <button className="btn text-white text-xl w-max px-10">Login</button>
-            </form>
-            <a className="text-blue-700 text-center text-sm" href="/login">
-              Forgot password?
-            </a>
-          </div>
+              <span className="block w-full text-2xl text-black uppercase font-bold mb-4">
+                Login
+              </span>
+              <form
+                className="mb-4 w-full flex flex-col items-center"
+                action="/"
+                method="post"
+              >
+                <div className="mb-4 w-full">
+                  <input
+                    className="input w-full border-b-2 p-2 outline-none focus:shadow-outline bg-gray-100"
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Username or Email"
+                  />
+                </div>
+                <div className="mb-6 w-full">
+                  <input
+                    className="input w-full border-b-2 p-2 outline-none focus:shadow-outline bg-gray-100"
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                  />
+                </div>
+                <button className="btn text-white text-xl w-max px-10">
+                  Login
+                </button>
+              </form>
+              <Link
+                className="text-blue-700 text-center text-sm"
+                to="/forgot-password"
+              >
+                Forgot Password?
+              </Link>
+              <Link
+                className="btn btn-primary  text-center text-sm mt-5"
+                to="/register"
+              >
+                Dont have an account? Register here
+              </Link>
+            </motion.div>
+          </AnimatePresence>
         </div>
       </div>
 
-      <div className="w-1/2 relative h-screen">
+      <motion.div
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5 }}
+        className="w-1/2 relative h-screen"
+      >
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-90"></div>
         <img
           className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-full w-full object-cover"
@@ -52,7 +78,7 @@ export default function LoginForm() {
           <MdComputer className="mr-4 mt-2" />
           TechReads
         </h1>
-      </div>
+      </motion.div>
     </div>
   );
 }
